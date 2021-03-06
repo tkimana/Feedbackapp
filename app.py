79f4c7191f0aa32bd7ce98acfe0172a28557c215
tkimana <kimana.theodore@gmail.com>
2021-03-05 20:25:49 -0600

    changes

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-    
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
